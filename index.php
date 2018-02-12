c37b5c62d08a683ead320746ba075fe568405446
Roan van der Duim <roanvanderduim1@gmail.com>
2018-02-12 18:11:48 +0100

    Set-up Basic index page with scripts and shit.
    jquery, bootstrap, jquery ui, font awesome, etc..

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <script src="assets/jquery/jquery-3.3.1.min.js"></script>
+   <script src="assets/bootstrap_js/bootstrap.min.js"></script>
+   <script src="assets/jquery_ui/jquery-ui.min.js"></script>
+   <script src="assets/font_awesome/all.js"></script>
+   <link rel="stylesheet" type="text/css" href="assets/jquery_ui/jquery-ui.min.css">
+   <link rel="stylesheet" type="text/css" href="assets/bootstrap_css/bootstrap.min.css">
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+ </head>
+  <body>
+
+    <i class="fa fa-volume-up"></i>
+
+  </body>
+</html>
