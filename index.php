75e6d5c7a213f14c6c263dd2a90f9ca312e3f00f
Roan van der Duim <roanvanderduim1@gmail.com>
2018-02-12 18:33:36 +0100

    Initial layout

--- index.php
+++ index.php
@@ -2,17 +2,33 @@
 <html>
  <head>
    <script src="assets/jquery/jquery-3.3.1.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
    <script src="assets/bootstrap_js/bootstrap.min.js"></script>
    <script src="assets/jquery_ui/jquery-ui.min.js"></script>
    <script src="assets/font_awesome/all.js"></script>
    <link rel="stylesheet" type="text/css" href="assets/jquery_ui/jquery-ui.min.css">
    <link rel="stylesheet" type="text/css" href="assets/bootstrap_css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" href="css/index.css">
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1">
  </head>
   <body>
 
-    <i class="fa fa-volume-up"></i>
+    <div class="wrapper">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1 class="text-center">Camel Dating</h1>
+          </div>
+        </div>
+      </div>
+    </div>
 
   </body>
 </html>
